e373c6c25f86852fe5037a6630e4218315729ed2
nazunamoe <nakashimayuki@kakao.com>
2021-03-21 14:31:21 +0900

    Revert "whyred: Add WhyredWifiOverlay"
    This reverts commit 743311adbc9de730bfcd9e162b9956663470cd63.

--- device.mk
+++ device.mk
@@ -98,9 +98,4 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
-# Wifi
-PRODUCT_PACKAGES += \
-    WhyredWifiOverlay
-
-# Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/whyred/whyred-vendor.mk)
