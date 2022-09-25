969e0d88d1055688028f62920d15cef2370cfe0e
nazunamoe <nakashimayuki@kakao.com>
2022-09-26 08:25:00 +0900

    whyred: CherishOS

--- cherish_whyred.mk
+++ cherish_whyred.mk
@@ -8,9 +8,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common superior stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 # Inherit from custom vendor.
 #$(call inherit-product, vendor/MiuiCamera/config.mk)
 
@@ -20,7 +17,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := whyred
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := lineage_whyred
+PRODUCT_NAME := cherish_whyred
 PRODUCT_MODEL := Redmi Note 5
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
@@ -39,13 +36,14 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
 
-# Ricedroid
-RICE_OFFICIAL := false
-RICE_MAINTAINER := nazunamoe
-
-# Boot Animation
-SUSHI_BOOTANIMATION := 1080
-
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_ENABLE_BLUR := true
+# Inherit some common CHERISH stuff.
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
+CHERISH_BUILD_TYPE := UNOFFICIAL
 TARGET_SUPPORTS_QUICK_TAP := true
+WITH_GMS := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.cherish.maintainer=nazunamoe
